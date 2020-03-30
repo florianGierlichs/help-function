2a1b8a9bee3b1536681995223fd1db0c37142b24
florianGierlichs <gierlichs@gmx.de>
2020-03-30 12:32:26 +0200

    Add copy to clipboard

---
+++ helpFunctions.js
@@ -0,0 +1,9 @@
+// Clipbord
+export function copyToClipboard(value) {
+  const dummy = document.createElement("input");
+  document.body.appendChild(dummy);
+  dummy.value = value;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+}
